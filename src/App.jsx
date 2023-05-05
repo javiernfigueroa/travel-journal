93863d85a145eb8e3edb36685fd41b9597e908be
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-05-05 15:02:40 -0400

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Header from "./components/Header"
 import Card from "./components/Card"
+import Data from "./Data";
 
+const cards = Data.map((item) => {
+  return (
+    <Card
+      {...item}
+    />
+  );
+});
 
 function App() {
   return (
     <>
      <Header/>
-     <Card/>
+     <section>{cards}</section>
+   
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,27 @@
-export default function Card() {
+import { faCoffee, faLocation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Card(props) {
   return (
     <div className="card">
       <div className="card--image_container">
-        <img src="../images/p1.png" alt="" />
+        <img src={props.imageUrl} alt="" />
       </div>
       <div className="card--info_container">
-        <span>ic</span>
-        <span>Japan</span>
-        <a href="">
-          <span>View on Google Maps</span>
-        </a>
+        <div className="card--location-container">
+          <span className="card--location-icon">
+            <FontAwesomeIcon icon={faLocation} />
+          </span>
+          <span className="card--country">{props.location}</span>
+          <a href={props.googleMapsUrl}>
+            <span className="card--google">View on Google Maps</span>
+          </a>
+        </div>
 
-        <h1>Mount Fuji</h1>
-        <h5>12 Jan, 2021 - 24 Jan, 2021</h5>
+        <h1>{props.title}</h1>
+        <h5>{props.startDate} - {props.endDate}</h5>
         <p>
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
+          {props.description}
         </p>
       </div>
     </div>
